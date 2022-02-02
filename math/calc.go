7c1655593a35e6f48b691d01a1a61c9095fac0a7
masibw <masi19bw@gmail.com>
2022-02-02 10:41:29 +0900

    slow add func

--- math/calc.go
+++ math/calc.go
@@ -1,7 +1,25 @@
 package math
 
+import "math"
+
 func Add(a int, b int) int {
-	return a + b
+	sum := 0
+	for i := 0; i < int(math.Abs(float64(a))); i++ {
+		if a < 0 {
+			sum--
+		} else {
+			sum++
+		}
+
+	}
+	for i := 0; i < int(math.Abs(float64(b))); i++ {
+		if b < 0 {
+			sum--
+		} else {
+			sum++
+		}
+	}
+	return sum
 }
 
 func Minus(a int, b int) int {
